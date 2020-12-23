a768bb45f366e798e3e2680fcd25b13d03283c6f
peedromarinhow <peedromarinhow@gmail.com>
2020-12-23 07:56:28 -0300

    now cpp files go on the build dir

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 call "D:\ProgramData\VisualStudio\VC\Auxiliary\Build\vcvarsall.bat" x64
 REM I hate pink, so no .cpp files on the github repo are allowed
-copy src\*.c *.cpp
-copy src\*.h *.h
-
 pushd build\debug
+copy ..\..\src\*.c *.cpp
+copy ..\..\src\*.h *.h
+
 del *.pdb > NUL 2> NUL
-cl /Zi ..\..\win32_main.cpp user32.lib gdi32.lib
+cl /Zi win32_main.cpp user32.lib gdi32.lib
 popd
--- debug.bat
+++ debug.bat
@@ -1,3 +1,3 @@
-copy src\*.c *.cpp
-copy src\*.h .h
+copy src\*.c build\debug\*.cpp
+copy src\*.h build\debug\*.h
 devenv build\debug\win32_main.exe
