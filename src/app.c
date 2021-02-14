4c8905f5718bee65fe67c7b5a4da8eec1401044b
peedromarinhow <peedromarinhow@gmail.com>
2021-02-14 15:52:51 -0300

    vulkan is much harder than i expected

--- src/app.c
+++ src/app.c
@@ -22,6 +22,36 @@ void OutputSineWave(i16 *Samples, i32 SamplesPerSecond, i32 SampleCount,
     }
 }
 
+#if 0
+internal b32 HasExtensions(VkPhysicalDevice *Device, char **RequiredExtensions, u32 ExtensionCount) {
+    u32 DeviceExtensionCount = 0;
+    vkEnumerateDeviceExtensionProperties(Device, NULL, &DeviceExtensionCount, NULL);
+#define MAX_DEVICE_EXTENSION_COUNT 107
+    //todo: dynamically allocate memory
+    VkExtensionProperties Extensions[MAX_DEVICE_EXTENSION_COUNT/*DeviceExtensionCount*/] = {0};
+#undef  MAX_DEVICE_EXTENSION_COUNT
+    for (u32 ExtensionIndex;
+                ExtensionIndex < ExtensionCount;
+                ExtensionIndex++)
+    {
+        b32 HasExtension = 0;
+        for (u32 DeviceExtensionIndex = 0;
+                 DeviceExtensionIndex < DeviceExtensionCount;
+                 DeviceExtensionIndex++)
+        {
+            //todo: actually compare the strings of the device and the required extensions
+            //note: why are these strings?  
+        }
+    }
+}
+
+internal b32 GetQueuefamily(VkPhysicalDevice *Device, VkQueueFlags Flags, i32 QueueFamilyIndex) {
+    b32 Succes = 0;
+    u32 QueueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(*Device, &QueueFamilyCount, NULL);
+    VkQueueFamilyProperties QueueFamilies;
+}
+
 void VulkanTest(void) {
     VkInstance Instance = {0};
     VkApplicationInfo AppInfo = {0}; {
@@ -50,9 +80,39 @@ void VulkanTest(void) {
         Assert(!"NOOOOOOOOOO!!");
     }
 
+    u32 DeviceCount;
+    vkEnumeratePhysicalDevices(Instance, &DeviceCount, NULL);
+#define MAX_DEVICE_COUNT 1
+    //todo: dynamically allocate memory
+    VkPhysicalDevice Devices[MAX_DEVICE_COUNT/*DeviceCount*/] = {0};
+#undef  MAX_DEVICE_COUNT
+    vkEnumeratePhysicalDevices(Instance, &DeviceCount, Devices);
+
+    const u32 ExtensionCount = 1;
+    char *DeviceExtensions[ExtensionCount] = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+    for (u32 DeviceIndex = 0;
+             DeviceIndex < DeviceCount;
+             DeviceIndex++)
+    {
+        // if (HasRequiredEstensions(...))
+        //note: assuming that my card has VK_KHR_SWAPCHAIN_EXTENSION_NAME
+        {
+            if (GetQueuefamily(Devices[DeviceIndex], 0, 0))
+
+        }
+    }
+
     vkDestroyInstance(Instance, NULL);
 }
+#endif
+//note: vulkan is much harder than i expected
+
+//todo: APP_ONLOAD function that runs when the app is loaded
+//note:
+//  this will require app memory, so that the vulkan stuff
+//  can be stored in it and shared between the functions
 
 __declspec(dllexport) APP_UPDATE(AppUpdate) {
-    VulkanTest();
+    //
 }
