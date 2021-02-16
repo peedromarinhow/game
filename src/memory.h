a6cb446135f933b354cb36e7d26aa258e0e76b5b
peedromarinhow <peedromarinhow@gmail.com>
2021-02-16 19:45:44 -0300

    freetype rip

--- src/memory.h
+++ src/memory.h
@@ -5,14 +5,28 @@
 
 typedef struct _app_memory {
     void *Contents;
-    u64   Size;
+    u32   Size;
 } app_memory;
 
-typedef struct _arena {
-    void *Base;
-    u64   Max;
-    u64   AllocPos;
-    u64   CommitPos;
-} arena;
+typedef struct _memory_arena {
+    u32 Size;
+    u32 Used;
+    u8 *Base;
+} memory_arena;
+
+internal void InitializeArena(memory_arena *Arena, u32 Size, void *Base) {
+    Arena->Size = Size;
+    Arena->Base = Base;
+    Arena->Used = 0;
+}
+
+#define PushStructToArena(Arena, type) (type *)_PushStructToArena((Arena), sizeof(type))
+internal void *_PushStructToArena(memory_arena *Arena, u32 Size) {
+    Assert((Arena->Used + Size) <= Arena->Size);
+    void *Result = Arena->Base + Arena->Used;
+    Arena->Used += Size;
+
+    return Result;
+}
 
 #endif//MEMORY_H
