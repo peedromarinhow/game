63c98e485dd8e66b00ceb85ea874aea8c289c0f8
peedromarinhow <peedromarinhow@gmail.com>
2021-05-03 11:04:49 -0300

    primitive (and not working) syntax highlighting

--- src/text_editor/app.c
+++ src/text_editor/app.c
@@ -20,11 +20,459 @@ global platform_api GlobalPlatformApi;
 #include "colors.h"
 #include "app.h"
 
-internal void DrawToken(renderer *Renderer, ui_ctx *Ctx, ui_input *Input, id Font, color Color, rv2 Pos) {
+u8 CHAR_TO_DIGIT[128] = {
+    ['0'] = 0,
+    ['1'] = 1,
+    ['2'] = 2,
+    ['3'] = 3,
+    ['4'] = 4,
+    ['5'] = 5,
+    ['6'] = 6,
+    ['7'] = 7,
+    ['8'] = 8,
+    ['9'] = 9,
+    ['a'] = 10,
+    ['A'] = 10,
+    ['b'] = 11,
+    ['B'] = 11,
+    ['c'] = 12,
+    ['C'] = 12,
+    ['d'] = 13,
+    ['D'] = 13,
+    ['e'] = 14,
+    ['E'] = 14,
+    ['f'] = 15,
+    ['F'] = 15,
+};
 
+typedef enum _token_type {
+    TOKEN_TYPE_EOF,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_DELIMITER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_STR,
+    TOKEN_TYPE_INT,
+    TOKEN_TYPE_FLOAT,
+    TOKEN_TYPE_NAME
+} token_type;
+
+typedef enum _token_mode {
+    TOKEN_MODE_NONE,
+    TOKEN_MODE_HEX,
+    TOKEN_MODE_OCT,
+    TOKEN_MODE_BIN,
+    TOKEN_MODE_CHAR
+} token_mode;
+
+typedef struct _token {
+    token_type Type;
+    token_mode Mode;
+
+    u32 Error;
+
+    c8 *Start;
+    c8 *End;
+
+    union {
+        u64 iVal;
+        f64 fVal;
+        c8 *sVal;
+        c8 *Name;
+    };
+    u32 NameLen;
+} token;
+
+internal c8 *ScanInt(c8 *Stream, token *Token) {
+    Token->Start = Stream;
+    u64 Base = 10;
+    if (*Stream == '0') {
+        Stream++;
+        if (tolower(*Stream) == 'x') {
+            Stream++;
+            Token->Mode = TOKEN_MODE_HEX;
+            Base = 16;
+        }
+        else
+        if (tolower(*Stream) == 'b') {
+            Stream++;
+            Token->Mode = TOKEN_MODE_BIN;
+            Base = 2;
+        }
+        else
+        if (isdigit(*Stream)) {
+            Token->Mode = TOKEN_MODE_OCT;
+            Base = 8;
+        }
+    }
+    u64 Val = 0;
+    while (1) {
+        u64 Digit = CHAR_TO_DIGIT[*Stream];
+        if (Digit == 0 && *Stream != '0') {
+            break;
+        }
+        if (Digit > Base) {
+            //todo: switch color to some error color or use those squiggly undelines idk
+            Token->Error = 1;
+        }
+        if (Val > (UINT64_MAX - Digit)/Base) {
+            //note: see above todo
+            Token->Error = 1;
+            while (isdigit(*Stream))
+                Stream++;
+            Val = 0;
+        }
+        Val = Val * Base + Digit;
+        Stream++;
+    }
+    Token->End  = Stream;
+    Token->iVal = Val;
+    Token->Type = TOKEN_TYPE_INT;
+
+    return Stream;
 }
 
-internal void uiBottomBar(editor_context *c) {
+internal c8 *ScanFloat(c8 *Stream, token *Token) {
+    c8 *Start    = Stream;
+    Token->Start = Start;
+    while (isdigit(*Stream)) {
+        Stream++;
+    }
+    if (*Stream == '.') {
+        Stream++;
+    }
+    Stream++;
+    while (isdigit(*Stream)) {
+        Stream++;
+    }
+    if (tolower(*Stream) == 'e') {
+        Stream++;
+        if (*Stream == '+' || *Stream == '-') {
+            Stream++;
+        }
+        if (!isdigit(*Stream)) {
+            Token->Error = 1;
+        }
+        while (isdigit(*Stream)) {
+            Stream++;
+        }
+    }
+    c8 *End = Stream;
+    f64 Val = strtod(Start, NULL);
+    if (Val == HUGE_VAL || Val == -HUGE_VAL) {
+        Token->Error = 1;
+    }
+    Token->End  = Stream;
+    Token->fVal = Val;
+    Token->Type = TOKEN_TYPE_FLOAT;
+
+    return Stream;
+}
+
+char ESCAPE_TO_CHAR[128] = {
+    ['n'] = '\n',
+    ['r'] = '\r',
+    ['t'] = '\t',
+    ['v'] = '\v',
+    ['b'] = '\b',
+    ['a'] = '\a',
+    ['0'] =   0,
+};
+
+internal c8 *ScanStr(c8 *Stream, token *Token) {
+    Assert(*Stream == '"');
+    Token->Start = Stream;
+    Stream++;
+    char *Str = NULL;
+    while (*Stream && *Stream != '"') {
+        char Val = *Stream;
+        if (Val == '\n') {
+            Token->Error = 1;
+        }
+        else
+        if (Val == '\\') {
+            Stream++;
+            Val = ESCAPE_TO_CHAR[*Stream];
+            if (Val == 0 && *Stream != '0') {
+                Token->Error = 1;
+            }
+        }
+        Stream++;
+    }
+    if (*Stream) {
+        Assert(*Stream == '"');
+        Stream++;
+    }
+    else {
+        Token->Error = 1;
+    }
+    Token->End  = Stream;
+    Token->sVal = Str;
+    Token->Type = TOKEN_TYPE_STR;
+
+    return Stream;
+}
+
+internal c8 *ScanChar(c8 *Stream, token *Token) {
+    Token->Start = Stream;
+    Stream++;
+    c8 Val = 0;
+    if (*Stream == '\'') {
+        //
+        Stream++;
+    }
+    else
+    if (*Stream == '\n') {
+        //
+    }
+    else
+    if (*Stream == '\\') {
+        Stream++;
+        Val = ESCAPE_TO_CHAR[*Stream];
+        if (Val == 0 && *Stream != '0') {
+            //
+        }
+        Stream++;
+    }
+    else {
+        Val = *Stream;
+        Stream++;
+    }
+    
+    if (*Stream != '\'') {
+        //
+    }
+    else {
+        Stream++;
+    }
+    Token->End  = Stream;
+    Token->iVal = Val;
+    Token->Type = TOKEN_TYPE_INT;
+    Token->Mode = TOKEN_MODE_CHAR;
+
+    return Stream;
+}
+
+#define CASE1(c, c1, k)             \
+    case c: {                       \
+        Token->Type = *Stream++;    \
+        if (*Stream == c1) {        \
+            Token->Type = k;        \
+            Stream++;               \
+        }                           \
+        break;                      \
+    }
+
+#define CASE2(c, c1, k, c2, k1)     \
+    case c: {                       \
+        Token->Type = *Stream++;    \
+        if (*Stream == c1) {        \
+            Token->Type = k;        \
+            Stream++;               \
+        }                           \
+        else                        \
+        if (*Stream == c2) {        \
+            Token->Type = k1;       \
+            Stream++;               \
+        }                           \
+        break;                      \
+    }
+
+i32 IsKeyword(c8 *Name) {
+    return 0;
+}
+
+internal c8 *GetSubStr(c8 *Start, c8 *End) {
+    u32 Len = (i32)(End - Start);
+    c8 *Str = GlobalPlatformApi.AllocateMemory(Len + 1);
+    CopyMemory(Str, Start, Len);
+    Str[Len] = 0;
+    return Str;
+}
+
+internal c8 *NextToken(c8 *Stream, token *Token) {
+TOP:
+    Token->Start = Stream;
+    Token->Mode  = 0;
+    switch (*Stream) {
+        case ' ': case '\n': case '\r': case '\t': case '\v': {
+            while (isspace(*Stream)) {
+                Stream++;
+            }
+            goto TOP;
+            break;
+        }
+        case '\'': {
+            Stream = ScanChar(Stream, Token);
+            break;
+        }
+        case '"': {
+            Stream = ScanStr(Stream, Token);
+            break;
+        }
+        case '.': {
+            Stream = ScanFloat(Stream, Token);
+            break;
+        }
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9': {
+            while (isdigit(*Stream)) {
+                Stream++;
+            }
+            c8 c = *Stream;
+            Stream = Token->Start;
+            if (c == '.' || tolower(c) == 'e') {
+                Stream = ScanFloat(Stream, Token);
+            }
+            else {
+                Stream = ScanInt(Stream, Token);
+            }
+            break;
+        }
+        case 'a': case 'b': case 'c': case 'd': case 'e':
+        case 'f': case 'g': case 'h': case 'i': case 'j':
+        case 'k': case 'l': case 'm': case 'n': case 'o':
+        case 'p': case 'q': case 'r': case 's': case 't':
+        case 'u': case 'v': case 'w': case 'x': case 'y':
+        case 'z':
+        case '_': {
+            c8 *Start = Stream++;
+            while (isalnum(*Stream) || *Stream == '_') {
+                Stream++;
+            }
+            Token->Name    = Token->Start;
+            Token->NameLen = Stream - Token->Start;
+            Token->Type = IsKeyword(Token->Name) ? TOKEN_TYPE_KEYWORD : TOKEN_TYPE_NAME;
+            break;
+        }
+        case '<': {
+            Token->Type = *Stream++;
+            if (*Stream == '<') {
+                Token->Type = TOKEN_TYPE_OPERATOR;//TOKEN_TYPE_LSHIFT;
+                Stream++;
+                if (*Stream == '=') {
+                    Token->Type = TOKEN_TYPE_OPERATOR;//TOKEN_TYPE_LSHIFT_ASSIGN;
+                    Stream++;
+                }
+            }
+            else
+            if (*Stream == '=') {
+                Token->Type = TOKEN_TYPE_OPERATOR;//TOKEN_TYPE_LTEQ;
+                Stream++;
+            }
+            break;
+        }
+        case '>': {
+            Token->Type = *Stream++;
+            if (*Stream == '>') {
+                Token->Type = TOKEN_TYPE_OPERATOR;//TOKEN_TYPE_RSHIFT;
+                Stream++;
+                if (*Stream == '=') {
+                    Token->Type = TOKEN_TYPE_OPERATOR;//TOKEN_TYPE_RSHIFT_ASSIGN;
+                    Stream++;
+                }
+            }
+            else
+            if (*Stream == '=') {
+                Token->Type = TOKEN_TYPE_OPERATOR;//TOKEN_TYPE_GTEQ;
+                Stream++;
+            }
+            break;
+        }
+        CASE1('&', '=', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_AND_ASSIGN*/)
+        CASE1('|', '=', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_OR_ASSIGN*/)
+        CASE1('^', '=', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_XOR_ASSIGN*/)
+        CASE1('*', '=', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_MUL_ASSIGN*/)
+        CASE1('/', '=', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_DIV_ASSIGN*/)
+        CASE1('%', '=', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_MOD_ASSIGN*/)
+        CASE1('=', '=', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_EQ*/)
+        CASE1('!', '=', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_NOTEQ*/)
+        CASE2('+', '=', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_ADD_ASSIGN*/, '+', TOKEN_TYPE_OPERATOR/*TOKEN_TYPE_INC*/)
+        case '-': {
+            Token->Type = *Stream++;
+            if (*Stream == '=') {
+                Token->Type = TOKEN_TYPE_OPERATOR;//TOKEN_TYPE_MIN_ASSIGN;
+                Stream++;
+                if (*Stream == '-') {
+                    Token->Type = TOKEN_TYPE_OPERATOR;//TOKEN_TYPE_DEC;
+                    Stream++;
+                }
+            }
+            else
+            if (*Stream == '>') {
+                Token->Type = TOKEN_TYPE_OPERATOR;//TOKEN_TYPE_ARR;
+                Stream++;
+            }
+            break;
+        }
+        default: {
+            Token->Type = *Stream++;
+            break;
+        }
+    }
+    Token->End = Stream;
+    return Stream;
+}
+
+internal void DrawScannedBuffer(editor_context *c, c8 *Stream) {
+    ui_ctx   *Ctx      = c->uiCtx;
+    ui_style *Style    = c->uiStyle;
+    ui_input *Input    = c->uiInput;
+    renderer *Renderer = c->Renderer;
+
+    font *Font = &Renderer->Fonts[Style->MonoFont];
+
+    rv2 Pos = rv2_(Style->Padding.x, Renderer->TargetClipRect.h - (Style->Padding.y*2 + Renderer->Fonts[Style->MainFont].Height));
+    r32 TempX = Pos.x;
+
+    token Token = {0};
+
+    u32 Index      =  0;
+    rv2 Offset     = {0};
+    rect GlyphRect = {0};
+
+    colorb Color = GREY_50;
+
+    Stream = NextToken(Stream, &Token);
+    while (Token.Type != TOKEN_TYPE_EOF) {
+
+        if (Token.Type == TOKEN_TYPE_EOF)
+            Color = ORANGE_600;
+        if (Token.Type == TOKEN_TYPE_OPERATOR)
+            Color = GREY_600;
+        if (Token.Type == TOKEN_TYPE_DELIMITER)
+            Color = GREY_600;
+        if (Token.Type == TOKEN_TYPE_KEYWORD)
+            Color = BLUE_600;
+        if (Token.Type == TOKEN_TYPE_STR)
+            Color = GREEN_600;
+        if (Token.Type == TOKEN_TYPE_INT)
+            Color = RED_600;
+        if (Token.Type == TOKEN_TYPE_FLOAT)
+            Color = RED_600;
+        if (Token.Type == TOKEN_TYPE_NAME)
+            Color = BLUE_600;
+
+        for (c8 *Char = Token.Start; Char < Token.End; Char++) {
+            Index  = (*Char - 32 >= 0)? *Char - 32 : '?' - 32;
+            Offset = Font->GlyphOffsets[Index];
+            GlyphRect = rect_(Pos.x + Offset.x, Pos.y - Offset.y, GetVecComps(Font->GlyphRects[Index].Dim));
+
+            DrawGlyph(Renderer, Style->MonoFont, Index, GlyphRect.Pos, Color);
+            Pos.x += Font->GlyphAdvances[Index];
+        }
+        Stream = NextToken(Stream, &Token);
+    }
+}
+
+internal void DrawBottomBar(editor_context *c) {
     buffer   *Buffer   = c->Buffers[c->CurrentBuffer];
     ui_ctx   *Ctx      = c->uiCtx;
     ui_style *Style    = c->uiStyle;
@@ -160,9 +608,10 @@ internal void DrawBuffer(editor_context *c) {
 
 global r32 y = 0;
 
-void DrawUi(editor_context *c) {
-    DrawBuffer(c);
-    uiBottomBar(c);
+internal void DrawUi(editor_context *c, c8 *Stream) {
+    // DrawBuffer(c);
+    DrawScannedBuffer(c, Stream);
+    DrawBottomBar(c);
 
     // file_group Tabs = GlobalPlatformApi.GetDirFilenames("D:/code/etc/etcetc/*.c");
     // c8 PathBuffer[256];
@@ -187,12 +636,12 @@ internal void SetKeymap() {
     Keymap[KEY_RIGHT] = command_(cmd_proc_MoveCarretRight, "MoveCarretRight");
     Keymap[KEY_CTRL | KEY_LEFT]  = command_(cmd_proc_MoveCarretToPrevToken, "MoveCarretToPrevToken");
     Keymap[KEY_CTRL | KEY_RIGHT] = command_(cmd_proc_MoveCarretToNextToken, "MoveCarretToNextToken");
-    Keymap[KEY_UP]    = command_(cmd_proc_MoveCarretUp, "MoreCarretUp");
-    Keymap[KEY_DOWN]  = command_(cmd_proc_MoveCarretDown, "MoveCarretDown");
-    Keymap[KEY_HOME]  = command_(cmd_proc_MoveCarretToLineStart, "MoveCarretToLineStart");
-    Keymap[KEY_END]   = command_(cmd_proc_MoveCarretToLineEnd, "MoveCarretToLineEnd");
-    Keymap[KEY_CTRL | KEY_HOME]  = command_(cmd_proc_MoveCarretToBufferStart, "MoveCarretToLineStart");
-    Keymap[KEY_CTRL | KEY_END]   = command_(cmd_proc_MoveCarretToBufferEnd,   "MoveCarretToLineEnd");
+    Keymap[KEY_UP]   = command_(cmd_proc_MoveCarretUp, "MoreCarretUp");
+    Keymap[KEY_DOWN] = command_(cmd_proc_MoveCarretDown, "MoveCarretDown");
+    Keymap[KEY_HOME] = command_(cmd_proc_MoveCarretToLineStart, "MoveCarretToLineStart");
+    Keymap[KEY_END]  = command_(cmd_proc_MoveCarretToLineEnd, "MoveCarretToLineEnd");
+    Keymap[KEY_CTRL | KEY_HOME] = command_(cmd_proc_MoveCarretToBufferStart, "MoveCarretToLineStart");
+    Keymap[KEY_CTRL | KEY_END]  = command_(cmd_proc_MoveCarretToBufferEnd,   "MoveCarretToLineEnd");
     Keymap[KEY_RETURN] = command_(cmd_proc_InsertNewLine, "InsertNewLine");
     Keymap[KEY_CTRL | 'S'] = command_(cmd_proc_SaveFile, "SaveFile");
     Keymap[KEY_CTRL | 'O'] = command_(cmd_proc_OpenFile, "OpenFile");
@@ -215,12 +664,12 @@ typedef struct _app_state {
     renderer       Renderer;
     editor_context Context;
     memory_arena   Arena;
+
+    c8 *HackLexTextStream;
+    token HackLexTextToken;
 } app_state;
 
 external APP_INIT(Init) {
-    //todo:
-    //  do all these allocations from some memory pool
-    //  because this is getting out of hand.
     Assert(sizeof(app_state) <= p->Memory.Size);
     app_state *State = (app_state *)p->Memory.Contents;
 
@@ -254,6 +703,8 @@ external APP_INIT(Init) {
     State->Context.uiStyle->HotColor     = GREY_700;
     State->Context.uiStyle->DefaultColor = GREY_800;
 
+    State->HackLexTextStream = GlobalPlatformApi.LoadFile("a.c").Data;
+
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
@@ -264,7 +715,7 @@ external APP_UPDATE(Update) {
     app_state *State = (app_state *)p->Memory.Contents;
 
     UpdateEditorContextInput(&State->Context, p);
-    DrawUi(&State->Context);
+    DrawUi(&State->Context, State->HackLexTextStream);
     if (Keymap[State->Context.LastKeyComb].Proc)
         Keymap[State->Context.LastKeyComb].Proc(&State->Context);
     Render(&State->Renderer, p->WindowDim, GREY_900);
@@ -333,7 +784,8 @@ internal void *_GrowBuffer(const c8 *Buffer, u32 NewLen, u32 ElementSize) {
     buffer_header *NewHeader;
     if (Buffer) {
         NewHeader = xrealloc(_BufferHeader(Buffer), NewSize);
-    } else {
+    }
+    else {
         NewHeader = xmalloc(NewSize);
         NewHeader->Len = 0;
     }
